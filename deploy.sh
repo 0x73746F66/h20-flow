97f248f4c9e967f2bbb52bdc8fa7db548b53e8af
chrisdlangton <chrislangton84@gmail.com>
2017-12-21 21:55:28 +1100

    0.1.1

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+VERSION=$1
+
+if [ -z ${VERSION} ]; then
+    echo Version not set
+    exit 1
+fi
+
+git status
+
+echo -e "Continue? (Ctrl+C to cancel)"
+read
+
+git add .
+git commit -m "Version ${VERSION}\n$2" && \
+    git push github master --tags ${VERSION}
+
+sudo docker build . --force-rm --rm -t chrisdlangton/h2o-flow:${VERSION} --compress && \
+    sudo docker push chrisdlangton/h2o-flow:${VERSION} && \
+    sudo docker push chrisdlangton/h2o-flow:latest && \
+    echo ok && exit
+
+echo Failed
+exit 1
